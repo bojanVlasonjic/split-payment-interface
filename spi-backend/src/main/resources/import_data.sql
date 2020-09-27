3df9d96434ff24a65d3f2859d45b641acb570e50
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2020-09-28 00:39:24 +0200

    Dodat opis diplomskog rada

--- spi-backend/src/main/resources/import_data.sql
+++ spi-backend/src/main/resources/import_data.sql
@@ -1,9 +1,9 @@
 insert into app_user(id, email, password) values (-1, "user1@email.com", "password");
 insert into app_user(id, email, password) values (-2, "user2@email.com", "password");
 
-insert into account(id, number, recipient_name, recipient_address, creator_id) values (-1, "111-11-11", "PDV", "Blabla 2", -1);
-insert into account(id, number, recipient_name, recipient_address, creator_id) values (-2, "222-22-22", "Donation", "Blabla 1", -1);
-insert into account(id, number, recipient_name, recipient_address, creator_id) values (-3, "333-33-33", "Research", "Blabla 3", -1);
+insert into account(id, number, recipient_name, recipient_address, creator_id) values (-1, "111-11-11", "PDV", "Some address 1", -1);
+insert into account(id, number, recipient_name, recipient_address, creator_id) values (-2, "222-22-22", "Donation", "Some address 2", -1);
+insert into account(id, number, recipient_name, recipient_address, creator_id) values (-3, "333-33-33", "Research", "Some address 3", -1);
 
 insert into account(id, number, recipient_name, recipient_address, creator_id) values (-4, "444-44-44", "Breast cancer research", "Address 1", -2);
 
